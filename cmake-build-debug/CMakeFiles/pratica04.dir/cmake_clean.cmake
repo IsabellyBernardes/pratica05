61049a2f9766cbd19e947c90990e6f9cad5058f8
IsabellyBernardes <isabelly789.cb@gmail.com>
2025-01-13 09:59:31 -0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/pratica04.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pratica04.dir/fila_teste.cpp.obj"
+  "CMakeFiles/pratica04.dir/fila_teste.cpp.obj.d"
+  "CMakeFiles/pratica04.dir/impressora.cpp.obj"
+  "CMakeFiles/pratica04.dir/impressora.cpp.obj.d"
+  "CMakeFiles/pratica04.dir/lista_char.cpp.obj"
+  "CMakeFiles/pratica04.dir/lista_char.cpp.obj.d"
+  "CMakeFiles/pratica04.dir/lista_teste.cpp.obj"
+  "CMakeFiles/pratica04.dir/lista_teste.cpp.obj.d"
+  "CMakeFiles/pratica04.dir/main.cpp.obj"
+  "CMakeFiles/pratica04.dir/main.cpp.obj.d"
+  "CMakeFiles/pratica04.dir/pilha_teste.cpp.obj"
+  "CMakeFiles/pratica04.dir/pilha_teste.cpp.obj.d"
+  "CMakeFiles/pratica04.dir/polonesa.cpp.obj"
+  "CMakeFiles/pratica04.dir/polonesa.cpp.obj.d"
+  "libpratica04.dll.a"
+  "pratica04.exe"
+  "pratica04.exe.manifest"
+  "pratica04.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pratica04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
