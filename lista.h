299e715b2d31b06b05ddb71ce512c75de09a8b60
IsabellyBernardes <113481194+IsabellyBernardes@users.noreply.github.com>
2025-02-01 01:46:19 +0000

    sem espaço

--- lista.h
+++ lista.h
@@ -7,22 +7,12 @@
 template <typename T>
 class Lista {
 public:
-    // Método virtual puro para inserir um item
+    
     virtual void insere(const T& item) = 0;
-
-    // Método virtual puro para obter um item pelo índice
     virtual T pega(int idx) const = 0;
-
-    // Método virtual puro para remover um item pelo índice
     virtual void remove(int idx) = 0;
-
-    // Método virtual puro para exibir os itens da lista
     virtual void exibe() const = 0;
-
-    // Método virtual puro para obter o tamanho da lista
     virtual int tamanho() const = 0;
-
-    // Destrutor virtual
     virtual ~Lista() {}
 };
 
